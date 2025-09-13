58f5409601abeee70451c20d44ccac9c9ee56766
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-09-13 20:09:01 +0530

    Time: 3 ms (66.19%) | Memory: 17.9 MB (41.88%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -5,21 +5,37 @@ public:
         int n=queries.size();
         vector<int> ans(n);
         //1)sort nums
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); //TC =O(mlogm) , SC=O(logm)
         //now find pre sum
         for(int i=1;i<m;i++){
             nums[i]+=nums[i-1];
         }
         //now compare it to query elements
+        //TC=(m*n)
+        // for(int i=0;i<n;i++){
+        //     int count=0;
+        //     for(int j=0;j<m;j++){
+        //         if(queries[i]>=nums[j]) count++;
+        //         else break;
+        //     }
+        //     ans[i]=count;
+        // }  this can be done using binary search
+
         for(int i=0;i<n;i++){
-            int count=0;
-            for(int j=0;j<m;j++){
-                if(queries[i]>=nums[j]) count++;
-                else break;
+            int maxlen=0;
+            int lo=0;
+            int hi=m-1;
+            while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                if(nums[mid]>queries[i]) hi=mid-1;
+                else{
+                    maxlen=mid+1;
+                    lo=mid+1;
+                }
             }
-            ans[i]=count;
+            ans[i]=maxlen;
         }
-        return ans;
+        return ans; //complete TC=O(n*m + mlogm) = O(m*(n+logn))~=O(m*n) to be more optimised we can use Binary search as we are sorting the given arr
         
     }
 };
