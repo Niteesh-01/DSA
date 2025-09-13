d6268282e8095e26e54cb9bd04ce05151ab8b420
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-09-13 19:53:28 +0530

    Time: 23 ms (11.50%) | Memory: 17.8 MB (84.31%) - LeetSync

---
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int m=nums.size();
+        int n=queries.size();
+        vector<int> ans(n);
+        //1)sort nums
+        sort(nums.begin(),nums.end());
+        //now find pre sum
+        for(int i=1;i<m;i++){
+            nums[i]+=nums[i-1];
+        }
+        //now compare it to query elements
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<m;j++){
+                if(queries[i]>=nums[j]) count++;
+            }
+            ans[i]=count;
+        }
+        return ans;
+        
+    }
+};
