3da2e143dddb033dd05d2c145c8c7c21e05a8e62
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-10-05 00:39:13 +0530

    Time: 6 ms (47.61%) | Memory: 28.4 MB (89.52%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(nums[n/2 -2]==nums[n/2 -1]) return nums[n/2 -1];
+        else if(nums[n/2 + 1]==nums[n/2]) return nums[n/2+1];
+        return nums[nums.size()/2];
+    }
+};
