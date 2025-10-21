0e6fba6bb04f3fa876ac74ee3a36e007d4ef1902
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-10-21 22:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (62.63%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -15,19 +15,36 @@ public:
         ListNode* tempB=b;
         ListNode* c=new ListNode(100);
         ListNode* tempC=c;
-        while(tempA!=NULL && tempB!=NULL){
-            if(tempA->val<=tempB->val){
-                tempC->next=tempA;
-                tempA=tempA->next;
+        // while(tempA!=NULL && tempB!=NULL){ //this logic takes O(m+n) space complexity
+        //     if(tempA->val<=tempB->val){
+        //         tempC->next=tempA;
+        //         tempA=tempA->next;
+        //     }
+        //     else{
+        //         tempC->next=tempB;
+        //         tempB=tempB->next;
+        //     }
+        //     tempC=tempC->next;
+        // }
+        // if(tempA==NULL) tempC->next=tempB;
+        // else if(tempB==NULL) tempC->next=tempA;
+        // return c->next;
+
+        //O(1) solution 
+        while(a!=NULL && b!=NULL){
+            if(a->val<=b->val){
+                tempC->next=a; //a or tempA
+                a=a->next;
+                tempC=tempC->next;
             }
             else{
-                tempC->next=tempB;
-                tempB=tempB->next;
+                tempC->next=b;
+                b=b->next;
+                tempC=tempC->next;
             }
-            tempC=tempC->next;
         }
-        if(tempA==NULL) tempC->next=tempB;
-        else if(tempB==NULL) tempC->next=tempA;
+        if(a==NULL) tempC->next=b;
+        else if(b==NULL) tempC->next=a;
         return c->next;
     }
 };
