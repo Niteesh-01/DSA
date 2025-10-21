c3a7f0819bbdd15d16d14347835c1906946a6c6a
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-10-21 17:14:00 +0530

    Time: 4 ms (17.21%) | Memory: 22 MB (13.41%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -6,14 +6,6 @@ public:
         for(int i=0;i<n;i++){
             if(nums[i]!=i) return i;
         }
-        // int lo=0,hi=n-1;
-        // while(lo<=hi){
-        //     int mid=lo+(hi-lo)/2;
-        //     if(nums[mid]!=mid) return mid;
-        //     if(nums[mid]<mid) hi=mid-1;
-        //     if(nums[mid]==mid) lo=mid+1;
-        //     //else return ;
-        // }
         return n;
     }
 };
