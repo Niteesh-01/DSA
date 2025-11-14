de1319d389f9e8ef52898c65754240652eff6009
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-11-15 00:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (19.42%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -11,20 +11,41 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-        ListNode* curr=head->next;
-        ListNode* pre=head;
-        while(curr){
-                int a=curr->val;
-                int b=pre->val;
-                pre->val=a;
-                curr->val=b;
-            if(curr->next && curr->next->next){
-                pre=curr->next;
-                curr=pre->next;
-            }
-            else return head;
+        // if(head==NULL || head->next==NULL) return head;
+        // ListNode* curr=head->next;
+        // ListNode* pre=head;
+        // while(curr){
+        //         int a=curr->val;
+        //         int b=pre->val;
+        //         pre->val=a;
+        //         curr->val=b;
+        //     if(curr->next && curr->next->next){
+        //         pre=curr->next;
+        //         curr=pre->next;
+        //     }
+        //     else return head;
+        // }
+        // return head;
+
+        //above code swaping the node values and not the nodes so to swap the nodes:
+
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+
+        while(prev->next && prev->next->next) {
+            ListNode* first=prev->next;
+            ListNode* second=first->next;
+
+            // Swapping nodes
+            first->next=second->next;
+            second->next=first;
+            prev->next=second;
+
+            // Move prev two nodes ahead
+            prev=first;
         }
-        return head;
+        return dummy->next;
     }
 };
