52ce2b704f703f15635ed0e480d6c865648a8f62
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-11-16 14:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (86.03%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int num(char s){
-        int a=0;
-        if(s=='I') a=1;
-        else if(s=='V') a=5;
-        else if(s=='X') a=10;
-        else if(s=='C') a=100;
-        else if(s=='L') a=50;
-        else if(s=='D') a=500;
-        else a=1000;
-        return a;
+        if(s=='I') return 1;
+        else if(s=='V') return 5;
+        else if(s=='X') return 10;
+        else if(s=='C') return 100;
+        else if(s=='L') return 50;
+        else if(s=='D') return 500;
+        else return 1000;
     }
     int romanToInt(string s) {
         int ans=0 , idx=0;
