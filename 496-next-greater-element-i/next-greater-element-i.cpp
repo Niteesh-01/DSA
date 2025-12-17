a7f47704abca14725e84088c90e804c7a6f075d3
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-12-18 01:12:00 +0530

    Time: 3 ms (40.81%) | Memory: 12.7 MB (59.30%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nextG(nums2.size(),-1);
         stack<int> s;
+        unordered_map<int,int> m;
         for(int i=nums2.size()-1;i>=0;i--){
             while(s.size()>0 && s.top()<=nums2[i]){
                 s.pop();
             }
             if(s.empty()){
-                nextG[i]=-1;  //means next greater of the number is not exist
+                m[nums2[i]]=-1;  //means next greater of the number is not exist
             }
             else{ // s.top()>nums[i]
-                nextG[i]=s.top();  // s.top nums[i] ka next greater h 
+                m[nums2[i]]=s.top();  // s.top nums[i] ka next greater h 
             }
             s.push(nums2[i]);
         }
 
-        //now map them
-        unordered_map<int,int> m;
-        for(int i=0;i<nums2.size();i++){
-            m[nums2[i]]=nextG[i];
-        }
+        //now map them -> this can be done in above loop
+        // unordered_map<int,int> m;
+        // for(int i=0;i<nums2.size();i++){
+        //     m[nums2[i]]=nextG[i];
+        // }
+        
         //map kr liya ab nums1 ke ele ko map me search kro ar us se map ki hui value ko ans me push kro
         vector<int> ans;
         for(int i=0;i<nums1.size();i++){
