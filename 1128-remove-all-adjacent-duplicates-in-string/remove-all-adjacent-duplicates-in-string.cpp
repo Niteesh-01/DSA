99f31d17657135d3c0fdc341e0d0d06e15f7c765
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-12-20 22:29:06 +0530

    Time: 3 ms (79.67%) | Memory: 13.6 MB (76.24%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,23 +1,36 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(st.empty() || st.top()!=s[i]){
-                st.push(s[i]);
-            }
-            else st.pop();
-        }
+        //using stack concept
+        // stack<char> st;
+        // for(int i=0;i<s.size();i++){
+        //     if(st.empty() || st.top()!=s[i]){
+        //         st.push(s[i]);
+        //     }
+        //     else st.pop();
+        // }
 
-        stack<char> temp;
-        while(!st.empty()){
-            temp.push(st.top());
-            st.pop();
-        }
-        string ans="";
-        while(!temp.empty()){
-            ans+=temp.top();
-            temp.pop();
+        // stack<char> temp;
+        // while(!st.empty()){
+        //     temp.push(st.top());
+        //     st.pop();
+        // }
+        // string ans="";
+        // while(!temp.empty()){
+        //     ans+=temp.top();
+        //     temp.pop();
+        // }
+        // return ans; 
+
+        //using string concept
+        string ans = "";
+        for (int i = 0; i < s.size(); i++) {
+            if(!ans.empty() && ans.back() == s[i]){
+                ans.pop_back();  
+            } 
+            else{
+                ans.push_back(s[i]); 
+            }
         }
         return ans;
     }
