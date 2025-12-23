1b067d855729cee7ca188ae476b274f556bff12f
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-12-23 15:59:06 +0530

    Time: 1 ms (83.18%) | Memory: 28.6 MB (18.96%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> ans(nums.size(),-1);
-        if(nums.size()==1) return ans;
-        for(int i=0;i<nums.size();i++){
-            int j=i+1;
-            if(i==nums.size()-1) j=0;
-            while(j!=i){
-                if(nums[j]>nums[i]){
-                    ans[i]=nums[j];
-                    break;
-                }
-                if(j<nums.size()) j++;
-                if(j==nums.size()) j=0;
+        int n=nums.size();
+        stack<int> st;
+        vector<int> ans(n,-1);
+        for(int i=0;i<2*n;i++){
+            int j=i%n;
+            while(!st.empty() && nums[j]>nums[st.top()]){
+                ans[st.top()]=nums[j];
+                st.pop();
             }
+            st.push(j);
         }
         return ans;
+
+
+        // brute force approach
+
+        // vector<int> ans(nums.size(),-1);
+        // if(nums.size()==1) return ans;
+        // for(int i=0;i<nums.size();i++){
+        //     int j=i+1;
+        //     if(i==nums.size()-1) j=0;
+        //     while(j!=i){
+        //         if(nums[j]>nums[i]){
+        //             ans[i]=nums[j];
+        //             break;
+        //         }
+        //         if(j<nums.size()) j++;
+        //         if(j==nums.size()) j=0;
+        //     }
+        // }
+        // return ans;
     }
 };
