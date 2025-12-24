7b3b6445e78c5ae77454bfd1ec533adb29bd0070
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2025-12-24 21:46:35 +0530

    Time: 43 ms (5.90%) | Memory: 23.2 MB (79.91%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,36 +1,61 @@
 class MinStack {
 public:
-    stack<int> st;
-    stack<int> helper;
+    //method 1 T.C O(1) S.C O(n)
+    // stack<int> st;
+    // stack<int> helper;
 
+    // MinStack() {
+        
+    // }
+    
+    // void push(int val) {
+    //     st.push(val);
+    //     if(helper.empty() || val<helper.top()){
+    //         helper.push(val);
+    //     }
+    //     else{
+    //         helper.push(helper.top());
+    //     }
+    // }
+    
+    // void pop() {
+    //     st.pop();
+    //     helper.pop();
+    // }
+    
+    // int top() {
+    //     return st.top();
+    // }
+    
+    // int getMin() {
+    //     return helper.top();
+    // }
+
+
+    //method 2 using vector
+    vector<int> v;
     MinStack() {
         
     }
     
     void push(int val) {
-        if(st.empty()){
-            helper.push(val);
-        }
-        else if(val<helper.top()){
-            helper.push(val);
-        }
-        else{
-            helper.push(helper.top());
-        }
-        st.push(val);
+       v.push_back(val);
     }
     
     void pop() {
-        st.pop();
-        helper.pop();
+        v.pop_back();
     }
     
     int top() {
-        return st.top();
+        return v[v.size()-1];
     }
     
     int getMin() {
-        return helper.top();
+        int mn=v[0];
+        for(int i=1;i<v.size();i++){
+            mn=min(mn,v[i]);
+        }
+        return mn;
     }
 };
 
