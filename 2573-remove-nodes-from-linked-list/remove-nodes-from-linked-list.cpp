6950759357c0a94773d211be9df525def4404639
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-01-02 20:10:48 +0530

    Time: 5 ms (66.64%) | Memory: 161.3 MB (90.95%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -10,22 +10,44 @@
  */
 class Solution {
 public:
+    ListNode *reverseList(ListNode *head){
+        ListNode *prev=NULL,*curr=head,*next=NULL;
+        while(curr!=NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*> st;
+        // stack<ListNode*> st;
+        // ListNode *temp=head;
+        // while(temp){
+        //     while( st.size()>0 && (temp->val)>(st.top()->val) ){
+        //         st.pop();
+        //     }
+        //     st.push(temp);
+        //     temp=temp->next;
+        // }
+        // ListNode* newhead=NULL;
+        // while(st.size()>0){
+        //     st.top()->next=newhead;
+        //     newhead=st.top();
+        //     st.pop();
+        // }
+        // return newhead;
+
+        //using next greater ele approach
+        head=reverseList(head);
         ListNode *temp=head;
-        while(temp){
-            while( st.size()>0 && (temp->val)>(st.top()->val) ){
-                st.pop();
-            }
-            st.push(temp);
-            temp=temp->next;
+        while(temp!=NULL && temp->next!=NULL){
+            if(temp->val > temp->next->val)
+                temp->next=temp->next->next;
+            else
+                temp=temp->next;
         }
-        ListNode* newhead=NULL;
-        while(st.size()>0){
-            st.top()->next=newhead;
-            newhead=st.top();
-            st.pop();
-        }
-        return newhead;
+        return reverseList(head);
     }
 };
