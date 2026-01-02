9d54fd994103ab461b4ef9b77f3a24d024b6e7c2
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-01-02 19:32:54 +0530

    Time: 20 ms (21.33%) | Memory: 34.1 MB (26.32%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -16,5 +16,24 @@ public:
         // if(nums[n/2 -2]==nums[n/2 -1]) return nums[n/2 -1];
         // else if(nums[n/2 + 1]==nums[n/2]) return nums[n/2+1];
         // return nums[nums.size()/2];
+
+        // int candidate=nums[0] , vote=1;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i]==candidate) vote++;
+        //     else vote--;
+        //     if(vote==0){
+        //         candidate=nums[i];
+        //         vote=1;
+        //     }
+        // }
+        // vote=0;
+        // for(int x : nums) {
+        //     if(x==candidate)
+        //         vote++;
+        // }
+
+        // if(vote>=nums.size() / 2)
+        //     return candidate;
+        // return -1;   //Boyer–Moore Majority Vote Algorithm when a ele occurs more than n/2 times not exactly n/2 times 
     }
 };
