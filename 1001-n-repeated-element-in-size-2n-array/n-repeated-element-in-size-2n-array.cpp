77a042cd4657fdeeab109adeafb0bc9c4edc9f31
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-01-02 19:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (53.60%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(auto x:mp){
-            if(x.second==n/2) return x.first;
-        }
-        return -1;
+        // unordered_map<int,int> mp;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto x:mp){
+        //     if(x.second==n/2) return x.first;
+        // }
+        // return -1;
 
         // sort(nums.begin(),nums.end());
         // int n=nums.size();
@@ -17,6 +17,14 @@ public:
         // else if(nums[n/2 + 1]==nums[n/2]) return nums[n/2+1];
         // return nums[nums.size()/2];
 
+        for(int i=0;i<nums.size()-2;i++){
+            if(nums[i]==nums[i+1] || nums[i]==nums[i+2]){
+                return nums[i];
+            }
+        }
+        return nums[nums.size()-1];
+        
+        //Boyer–Moore Majority Vote Algorithm
         // int candidate=nums[0] , vote=1;
         // for(int i=1;i<nums.size();i++){
         //     if(nums[i]==candidate) vote++;
