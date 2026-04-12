43d4df022082529d91c8abdba4e7b581bae28669
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-04-13 00:25:36 +0530

    Time: 4 ms (41.30%) | Memory: 146.8 MB (48.17%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -12,23 +12,34 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-        int level=0;
-        if(!root) return level;
-        queue<TreeNode*> q;
-        q.push(root);
-        level++;
+        //BFS
+        // int level=0;
+        // if(!root) return level;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // level++;
 
-        while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                TreeNode* temp=q.front();
-                q.pop();
-                if(!temp->left and !temp->right) return level;
-                if(temp->left) q.push(temp->left);
-                if(temp->right) q.push(temp->right);
-            }
-            level++;
-        }
-        return -1;
+        // while(!q.empty()){
+        //     int n=q.size();
+        //     while(n--){
+        //         TreeNode* temp=q.front();
+        //         q.pop();
+        //         if(!temp->left and !temp->right) return level;
+        //         if(temp->left) q.push(temp->left);
+        //         if(temp->right) q.push(temp->right);
+        //     }
+        //     level++;
+        // }
+        // return -1;
+
+        //DFS
+        if(!root) return 0;
+
+        if(root->left==NULL and root->right==NULL) return 1;
+
+        int L=root->left!=NULL ? minDepth(root->left) : INT_MAX;
+        int R=root->right!=NULL ? minDepth(root->right) : INT_MAX;
+
+        return 1+min(L,R);
     }
 };
