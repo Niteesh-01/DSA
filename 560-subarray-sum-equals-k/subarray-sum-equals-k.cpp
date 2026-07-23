bf60c70c7f1a381f7e8ed22bfb714d56c838a834
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-07-23 21:31:51 +0530

    Time: 79 ms (26.60%) | Memory: 53.9 MB (5.50%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        //brute force approach
-        int n=nums.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum+=nums[j];
-                if(sum==k) count++;
-            }
-        }
-        return count;
-
-        // int i=0,j=1,count=0;
-        // int sum=nums[0];
-        // while(i<=j && j<nums.size()){
-        //     if(sum==k){
-        //         count++;
-        //         sum=sum-nums[i]+nums[j];
-        //         i++;
-        //         j++;
-        //     }
-        //     else if(sum<k){
+        // //brute force approach
+        // int n=nums.size();
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     int sum=0;
+        //     for(int j=i;j<n;j++){
         //         sum+=nums[j];
-        //         j++;
-        //     }
-        //     else{
-        //         sum-=nums[i];
-        //         i++;
+        //         if(sum==k) count++;
         //     }
         // }
         // return count;
+
+        //optimal solution using prefix sum and hash map
+        map<int,int> m;
+        m[0]=1;
+        int psum=0,cnt=0;
+        for(int i=0;i<nums.size();i++){
+            psum+=nums[i];
+            int remove=psum-k;
+            cnt+=m[remove];
+            m[psum]+=1;
+        }
+        return cnt;
     }
 };
