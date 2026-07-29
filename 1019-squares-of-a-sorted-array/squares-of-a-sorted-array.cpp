199e2482fb77e00e235209d03b128f0267f1150d
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-07-29 19:11:31 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (29.59%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -9,37 +9,58 @@ public:
         // sort(ans.begin(),ans.end());
         // return ans;
 
-        vector<int> neg;
-        vector<int> pos;
-        vector<int> ans;
+        // vector<int> neg;
+        // vector<int> pos;
+        // vector<int> ans;
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]<0) neg.push_back(nums[i]);
-            else pos.push_back(nums[i]);
-        }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]<0) neg.push_back(nums[i]);
+        //     else pos.push_back(nums[i]);
+        // }
 
-        int i=neg.size()-1,j=0;
-        while(i>=0 && j<pos.size()){
-            int sq=neg[i]*neg[i];
-            int sq1=pos[j]*pos[j];
-            if(sq<sq1) {
-                ans.push_back(sq);
-                i--;
+        // int i=neg.size()-1,j=0;
+        // while(i>=0 && j<pos.size()){
+        //     int sq=neg[i]*neg[i];
+        //     int sq1=pos[j]*pos[j];
+        //     if(sq<sq1) {
+        //         ans.push_back(sq);
+        //         i--;
+        //     }
+        //     else{
+        //         ans.push_back(sq1);
+        //         j++;
+        //     }
+        // }
+        // // push the remainning elements of neg vector
+        // while(i>=0){
+        //     ans.push_back(neg[i]*neg[i]);
+        //     i--;
+        // }
+        // //push remaining of pos
+        // while(j<pos.size()){
+        //     ans.push_back(pos[j]*pos[j]);
+        //     j++;
+        // }
+        // return ans;
+
+
+        //without using extra spaces
+
+        vector<int> ans(nums.size(), 0);
+        int i=0;
+        int j=nums.size()-1;
+        int k=ans.size()-1;
+
+        while(i<=j){
+            if(abs(nums[i])<=abs(nums[j])){
+                ans[k]=nums[j]*nums[j];
+                j--;
             }
             else{
-                ans.push_back(sq1);
-                j++;
+                ans[k]=nums[i]*nums[i];
+                i++;
             }
-        }
-        // push the remainning elements of neg vector
-        while(i>=0){
-            ans.push_back(neg[i]*neg[i]);
-            i--;
-        }
-        //push remaining of pos
-        while(j<pos.size()){
-            ans.push_back(pos[j]*pos[j]);
-            j++;
+            k--;
         }
         return ans;
     }
