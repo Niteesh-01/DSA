c313862ac8094fea4af1ece865cc4e4fb09711db
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-07-29 19:03:32 +0530

    Time: 3 ms (51.54%) | Memory: 33 MB (5.54%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,12 +1,46 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n=nums.size();
+        // int n=nums.size();
+        // vector<int> ans;
+        // for(int i=0;i<n;i++){
+        //     ans.push_back(nums[i]*nums[i]);
+        // }
+        // sort(ans.begin(),ans.end());
+        // return ans;
+
+        vector<int> neg;
+        vector<int> pos;
         vector<int> ans;
-        for(int i=0;i<n;i++){
-            ans.push_back(nums[i]*nums[i]);
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<0) neg.push_back(nums[i]);
+            else pos.push_back(nums[i]);
         }
-        sort(ans.begin(),ans.end());
+
+        int i=neg.size()-1,j=0;
+        while(i>=0 && j<pos.size()){
+            int sq=neg[i]*neg[i];
+            int sq1=pos[j]*pos[j];
+            if(sq<sq1) {
+                ans.push_back(sq);
+                i--;
+            }
+            else{
+                ans.push_back(sq1);
+                j++;
+            }
+        }
+        // push the remainning elements of neg vector
+        while(i>=0){
+            ans.push_back(neg[i]*neg[i]);
+            i--;
+        }
+        //push remaining of pos
+        while(j<pos.size()){
+            ans.push_back(pos[j]*pos[j]);
+            j++;
+        }
         return ans;
     }
 };
