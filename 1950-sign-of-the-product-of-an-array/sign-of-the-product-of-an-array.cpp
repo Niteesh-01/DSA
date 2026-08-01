f341df2e9311f5b03f04ead869561b3962182cb1
Niteesh_Chaurasiya <nitishkumarchaurasiya3236@gmail.com>
2026-08-01 19:18:12 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (81.39%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int signF(long long x){
-        if(x>0) return 1;
-        if(x<0) return -1;
-        return 0;
-    }
     int arraySign(vector<int>& nums) {
-        long long p=1;
+        int neg=0,zero=0;
         for(int i=0;i<nums.size();i++){
-            p*=nums[i];
-            if(p>0) p=1;
-            if(p<0) p=-1;
+            if(nums[i]<0) neg++;
+            if(nums[i]==0) zero++;
         }
-        return signF(p);
+
+        if(zero>0) return 0;
+        if(neg%2==0) return 1;
+        return -1;
     }
 };
